19ad5a9e8b4053882099a7a5cd42bfeb268619e4
Rachel0211 <rachel0211s@gmail.com>
2015-11-02 01:30:46 +0900

    navigation mail service + 졸미 수정(선정)

--- app/src/main/java/kr/ac/kumoh/railroApplication/MainActivity.java
+++ app/src/main/java/kr/ac/kumoh/railroApplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.annotation.IdRes;
@@ -57,6 +58,7 @@ import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import kr.ac.kumoh.railroApplication.AdditionalService.NoticeActivity;
 import kr.ac.kumoh.railroApplication.KakaoService.RegisterAppActivity;
 import kr.ac.kumoh.railroApplication.classes.RealTimeLocationListener;
 import kr.ac.kumoh.railroApplication.classes.UseDB;
@@ -442,6 +444,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.share_menu_item:
                 setNewRootFragment(FloatingActionButtonFragment.newInstance());
                 break;
+
+            case R.id.drawer_notice:
+                Intent intent = new Intent(this, NoticeActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.drawer_service:
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                        "mailto", "rachel0211s@gmail.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "불만사항 메일입니다.");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "기종 :"+"\n"+ "오류사항(자세히 기재 해주세요) :"+"\n");
+                startActivity(Intent.createChooser(emailIntent, "내일이 고객센터 메일"));
+                break;
+
 
         }
         mCurrentMenuItem = id;
